8ebab1dcf61735a6e33462bacfd9b8d9567db857
Taylor Gorman <me@taylorpatrickgorman.com>
2015-11-16 14:03:15 -0600

    Added templates for image filters

--- plugins/this-website/this-website.php
+++ plugins/this-website/this-website.php
@@ -38,3 +38,37 @@ add_filter( 'bs_theme_support', function( $features ){
 	return $features;
 
 } );
+
+/*
+** Modify new image sizes
+**
+add_filter( 'bs_image_sizes', function( $sizes ){
+
+	// Prevent Blank Slate's default sizes
+	return false;
+
+	// Add new size
+	$sizes[] = array(
+		'slideshow' => array(
+			'width'  => 1280
+		,	'height' => 400
+		,	'crop'   => true
+		)
+	)
+	return $sizes;
+
+} );
+*/
+
+/*
+** Modify default image sizes
+**
+add_filter( 'bs_wp_image_sizes', function( $sizes ){
+
+	$sizes['thumbnail']['width']  = 200;
+	$sizes['thumbnail']['height'] = 200;
+	return $sizes;
+
+} );
+*/
+
