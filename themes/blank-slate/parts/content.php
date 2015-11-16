613c20725b704ae409724ee034b41c4bc3f78d3d
Taylor Gorman <me@taylorpatrickgorman.com>
2015-11-16 13:50:54 -0600

    Update: Only show excerpt on archive pages

--- themes/blank-slate/parts/content.php
+++ themes/blank-slate/parts/content.php
@@ -1,8 +1,12 @@
 <?
-if ( has_excerpt() )
-	echo '<p class="excerpt">'.get_the_excerpt().'</p>';
+if ( is_singular() ) {
 
-the_content();
-
-if ( is_singular() )
+	if ( has_excerpt() ) the_excerpt();
+	the_content();
 	edit_post_link('Edit this', '<p>','</p>');
+
+} else {
+
+	the_excerpt();
+
+}
