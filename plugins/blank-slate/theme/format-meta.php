31db424029ba4e37afeeb69dc84fd438ceb4f7f1
Taylor Gorman <me@taylorpatrickgorman.com>
2015-11-23 09:54:28 -0600

    Upgrade: Format meta
    Added source name, combined into one saved array

--- plugins/blank-slate/theme/format-meta.php
+++ plugins/blank-slate/theme/format-meta.php
@@ -21,6 +21,7 @@ add_action( 'add_meta_boxes', function() {
 
 } );
 
+
 /*
 ** Meta box content
 */
@@ -30,15 +31,26 @@ function format_meta_content( $post ) {
 	wp_nonce_field( 'format_meta_save_metabox', 'format_meta_nonce' );
 
 	// Get data
-	$permalink = get_post_meta( $post->ID, '_format_permalink', true );
-
+	$format_meta = wp_parse_args(
+		get_post_meta( $post->ID, 'format_meta', true )
+	, array(
+			'permalink' => ''
+		,	'source_name' => ''
+		)
+	);
 	?>
 	<table class="form-table">
 	<tr>
 		<th><label>Custom URL</label></th>
 		<td>
-			<input type="text" name="format_permalink" value="<?= $permalink ?>" class="regular-text">
+			<input type="text" name="format_meta[permalink]" value="<?= $format_meta['permalink'] ?>" class="regular-text">
 			<p class="description">This URL will override the permalink for this post. Include "http://" or the link will not work.</p>
+		</td>
+	</tr>
+	<tr>
+		<th><label>Source Name</label></th>
+		<td>
+			<input type="text" name="format_meta[source_name]" value="<?= $format_meta['source_name'] ?>" class="regular-text">
 		</td>
 	</tr>
 	</table>
@@ -57,6 +69,6 @@ add_action( 'save_post', function( $post_id ) {
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
 
 	// Save data
-	update_post_meta( $post_id, '_format_permalink', $_POST['format_permalink'] );
+	update_post_meta( $post_id, 'format_meta', $_POST['format_meta'] );
 
 } );
