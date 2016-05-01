0e3b2293b40fd7e8dc858964e43b68b794852def
Taylor Gorman <me@taylorpatrickgorman.com>
2016-05-01 14:16:34 -0500

    Post modification was modifying pages

--- plugins/blank-slate/structure/post.php
+++ plugins/blank-slate/structure/post.php
@@ -1,20 +1,9 @@
 <?php
 /*
 ** Modify Post post type
-
-class BS_Post_Post_Type extends BS_Post_Type {
-
-	protected static $id = 'post';
-
-	protected static $columns_to_remove = array('comments', 'tags');
-	protected static $remove_supports = array('trackbacks');
-
-	public static function register() {  }
-
-}
 */
 add_action( 'init', function(){
 
-	remove_post_type_support( 'page', 'trackbacks' );
+	remove_post_type_support( 'post', 'trackbacks' );
 
 } );
