849d34abebf58f86dce915f183c312b51b9952dd
Taylor Gorman <me@taylorpatrickgorman.com>
2016-05-15 20:27:58 -0500

    Ditch bs_human_timing, use WordPress's human_time_diff

--- plugins/blank-slate/functions/strings.php
+++ plugins/blank-slate/functions/strings.php
@@ -9,10 +9,10 @@
 function bs_isvg($args) {
 
 	$defaults = array(
-		'icon-name'	=> ''
-	,	'viewbox' 	=> '0 0 32 32'
-	,	'echo'		=> true
-	,	'path'      => bs_theme_url('/img/icons.svg')
+		'icon-name' => ''
+	,	'viewbox'   => '0 0 32 32'
+	,	'echo'      => true
+	,	'path'      => get_stylesheet_directory_uri().'/img/icons.svg'
 	);
 
 	$vars = wp_parse_args( $args, $defaults );
@@ -22,33 +22,6 @@ function bs_isvg($args) {
 	} else {
 		return $icon;
 	}
-}
-
-/**
- * Take a timestamp and turn it in to human timing.
- * @param  timestamp $time      To output, or not to output. Set to 0 if using URL-query.
- */
-function bs_human_timing ($time, $cutoff=2) {
-
-	$current_time = current_time('timestamp');
-    $time = $current_time - $time; // to get the time since that moment
-
-    $tokens = array (
-        31536000 => 'year',
-        2592000 => 'month',
-        604800 => 'week',
-        86400 => 'day',
-        3600 => 'hour',
-        60 => 'minute',
-        1 => 'second'
-    );
-
-    foreach ($tokens as $unit => $text) {
-        if ($time < $unit) continue;
-        $numberOfUnits = floor($time / $unit);
-        return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'');
-    }
-
 }
 
 /**
