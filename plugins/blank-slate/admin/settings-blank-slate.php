6e64596c404a51a3947a7f7721f188140fcd90af
Taylor Gorman <me@taylorpatrickgorman.com>
2016-07-10 01:21:47 -0500

    More options to Blank Slate Settings screen

--- plugins/blank-slate/admin/settings-blank-slate.php
+++ plugins/blank-slate/admin/settings-blank-slate.php
@@ -23,19 +23,56 @@ add_submenu_page( 'options-general.php', 'Blank Slate Settings', 'Blank Slate', 
 		<form method="post" action="options.php">
 		<?
 		settings_fields('blank_slate_settings');
-		$v = wp_parse_args( get_option('blank_slate_settings'), array(
-			'layout_classes' => false
-		) );
+		$v = wp_parse_args( get_option('blank_slate_settings') );
 		?>
 
 		<table class="form-table">
-		<tr>
-			<th>Layout Classes</th>
-			<td><label><input type="checkbox" name="blank_slate_settings[layout_classes]" value="1" <? checked($v['layout_classes']); ?>> Enable</label></td>
-		</tr>
+		<?
+		foreach( array(
+			array(
+				'type'   => 'checkbox'
+			,	'label'  => 'Layout Classes'
+			,	'values' => array(
+					'layouts_body' => 'Add to body classes'
+				,	'layouts_post' => 'Add to post classes'
+				)
+			)
+		,	array(
+				'type'   => 'checkbox'
+			,	'label'  => 'Post Formats'
+			,	'values' => array(
+					'formats_gallery' => 'Gallery'
+				,	'formats_quote'   => 'Quote'
+				,	'formats_etc'     => 'Etc'
+				)
+			)
+		,	array(
+				'type'   => 'checkbox'
+			,	'label'  => 'Scheduled Post Types'
+			,	'values' => array(
+					'scheduled_custom' => 'Custom PT'
+				,	'scheduled_etc'    => 'Etc'
+				)
+			)
+		) as $field ) {
+
+			?>
+			<tr>
+				<th><?= $field['label'] ?></th>
+				<td>
+					<p><? foreach ( $field['values'] as $name => $label ) { ?>
+					<label><input type="checkbox" name="blank_slate_settings[<?= $name ?>]" value="1" <? if ( ! empty($v[$name]) ) checked($v[$name]); ?>> <?= $label ?></label><br>
+					<? } ?></p>
+					<? if ( ! empty($field['desc']) ) echo '<p class="description">'. $field['desc'] .'</p>'; ?>
+				</td>
+			</tr>
+			<?
+
+		}
+		?>
 		</table>
 
-		<? submit_button(); ?>
+		<? submit_button(); echo '<pre>'.print_r($v,1).'</pre>'; ?>
 		</form>
 
 	</div>
