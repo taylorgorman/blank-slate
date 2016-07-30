edc33e23f928c0ce4368c15435c3e4f019cb12d8
Taylor Gorman <me@taylorpatrickgorman.com>
2016-07-30 17:44:25 -0500

    Reduce Layouts UI

--- plugins/blank-slate/admin/layouts.php
+++ plugins/blank-slate/admin/layouts.php
@@ -4,6 +4,11 @@
 */
 bs_register_taxonomy( array(
 	'singular_name' => 'layout'
-,	'plural_name' => 'layouts'
+,	'plural_name'   => 'layouts'
 ,	'post_types'    => get_post_types(array('show_ui'=>true))
+,	'arguments'     => array(
+		'show_in_menu'      => false
+	,	'show_in_nav_menus' => false
+	,	'show_admin_column' => false
+	)
 ) );
