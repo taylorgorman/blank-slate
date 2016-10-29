a2140c9b4f63acdd8c52a09dd1c5e16bb03e209b
Taylor Gorman <me@taylorpatrickgorman.com>
2016-10-29 16:19:28 -0500

    Bootstrap classes for nav menus

--- plugins/blank-slate/theme/wp_nav_menu.php
+++ plugins/blank-slate/theme/wp_nav_menu.php
@@ -31,19 +31,79 @@ add_filter( 'wp_nav_menu_args', function( $args ){
 
 } );
 
-/*
-** Strip <li>'s from wp_nav_menu HTML output
-*/
+add_filter( 'wp_nav_menu_objects', function( $sorted_menu_items, $args ){
+
+	// Not on admin
+	if ( is_admin() )
+		return;
+
+	foreach ( $sorted_menu_items as $item ) {
+
+		/*
+		** Menu items with hashlink
+		*/
+		$hashpos = strpos( $item->url, '#' );
+		if ( $hashpos !== false ) {
+
+			// Temporarily strip off hashlink
+			$url_wo_hash = substr( $item->url, 0, $hashpos );
+			//echo '<pre>'; var_dump($url_wo_hash); echo '</pre>';
+
+			// TODO: Strip domain from both URLs
+
+			// If we're at the item's URL, just display the hash
+			if ( $url_wo_hash === $_SERVER['REQUEST_URI'] )
+				$item->url = substr( $item->url, $hashpos );
+
+		}
+
+		/*
+		** Add Bootstrap dropdown classes
+		*/
+		if ( strpos($args->menu_class, 'navbar-') !== false ) {
+			if ( in_array( 'menu-item-has-children', $item->classes ) ) {
+				$item->classes[] = 'dropdown';
+			}
+		}
+
+	}
+
+	return $sorted_menu_items;
+
+}, 10, 2 );
+
 add_filter( 'wp_nav_menu_items', function( $items, $args ){
 
-	// Bootstrap: do nothing
-	if ( strpos($args->menu_class, 'navbar-') !== false )
-		return $items;
+	/*
+	** Add Bootstrap dropdown classes
+	*/
+	if ( strpos($args->menu_class, 'navbar-') !== false ) {
 
-	// Otherwise: remove li's
-	$find    = array( '><a', '</li>', '<li' );
-	$replace = array( '',    '',      '<a'  );
+		$items = str_replace( 'class="sub-menu', 'class="sub-menu dropdown-menu', $items );
 
-	return str_replace( $find, $replace, $items );
+		/* Uncomment to toggle dropdown on click with js
+		$parent_pos = strpos( $items, 'menu-item-has-children' );
+		while ( $parent_pos !== false ) {
+			$a_needle = '<a';
+			$a_pos = strpos( $items, $a_needle, $parent_pos);
+			$items = substr_replace( $items, ' class="dropdown-toggle" data-toggle="dropdown"', $a_pos + strlen($a_needle), 0 );
+			$parent_pos = strpos( $items, 'menu-item-has-children', $a_pos+1 );
+		}
+		*/
+
+	}
+
+	/*
+	** Strip <li>'s from wp_nav_menu HTML output
+	*/
+	else {
+
+		$find    = array( '><a', '</li>', '<li' );
+		$replace = array( '',    '',      '<a'  );
+		$items = str_replace( $find, $replace, $items );
+
+	}
+
+	return $items;
 
 }, 10, 2 );
