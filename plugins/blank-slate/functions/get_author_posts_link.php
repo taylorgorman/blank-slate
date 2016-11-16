970f4ae2d005ab1429d529d58460b73668d1c112
Taylor Gorman <me@taylorpatrickgorman.com>
2016-11-15 23:21:11 -0600

    Update: Add rel=author to author posts link

--- plugins/blank-slate/functions/get_author_posts_link.php
+++ plugins/blank-slate/functions/get_author_posts_link.php
@@ -6,9 +6,9 @@
 */
 function get_author_posts_link( $post = false ) {
 
-	if ( !is_object($post) )
+	if ( ! is_object($post) )
 		global $post;
 
-	return '<a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'">'. get_the_author_meta( 'display_name' ) .'</a>';
+	return '<a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'" rel="author">'. get_the_author_meta( 'display_name' ) .'</a>';
 
 }
